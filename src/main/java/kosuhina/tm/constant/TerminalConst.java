490cd61c70313b9001cdd23de7315015584ee6a2
Svetlana Kosuhina <lana__svet@list.ru>
2020-07-16 16:27:41 +0300

    Добавлен Apache Maven.

---
+++ src/main/java/kosuhina/tm/constant/TerminalConst.java
@@ -0,0 +1,9 @@
+package kosuhina.tm.constant;
+
+public class TerminalConst {
+
+    public static final String HELP = "help";
+    public static final String VERSION = "version";
+    public static final String ABOUT = "about";
+
+}
