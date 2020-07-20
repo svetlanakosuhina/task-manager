0374049be721243803291a5a9db28dfb198c1350
Svetlana Kosuhina <lana__svet@list.ru>
2020-07-20 15:30:35 +0300

    Реализована обработка ввода с клавиатуры. Реализована терминальная команда exit.

--- src/main/java/kosuhina/tm/App.java
+++ src/main/java/kosuhina/tm/App.java
@@ -1,64 +1,77 @@
 package kosuhina.tm;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 import static kosuhina.tm.constant.TerminalConst.*;
 
 public class App {
 
     public static void main(final String[] args) {
-       System.out.println(Arrays.toString(args));
-       run(args);
-	   displayWelcome();
+        run(args);
+        displayWelcome();
+        final Scanner scanner = new Scanner(System.in);
+        String command = "";
+        while (!EXIT.equals(command)) {
+            command = scanner.nextLine();
+            run(command);
+        }
     }
 
-    private static void run(final String[] args) {
+    public static void run(final String[] args) {
         if (args == null) return;
         if (args.length < 1) return;
         final String param = args[0];
+        final int result = run(param);
+        System.exit(result);
+    }
+
+
+    private static int run(final String param) {
+        if(param == null) return -1;
         switch (param) {
-            case VERSION:
-                displayVersion();
-            case ABOUT:
-                displayAbout();
-            case HELP:
-                displayHelp();
-            default:
-                displayError();
-            }
+            case VERSION: return displayVersion();
+            case ABOUT: return displayAbout();
+            case HELP: return displayHelp();
+            case EXIT: return displayExit();
+            default: return displayError();
         }
-
-
+    }
 
 
     private static void displayWelcome() {
         System.out.println("** WELCOME TO TASK MANAGER **");
     }
 
-    private static void displayHelp() {
+    private static int displayHelp() {
         System.out.println("version - Display application version.");
         System.out.println("about - Display developer info.");
         System.out.println("help - Display list of commands.");
-        System.exit( 0);
+        System.out.println("exit - Terminate console application.");
+        return 0;
     }
 
 
-    private static void displayVersion() {
+    private static int displayVersion() {
         System.out.println("1.0.0");
-        System.exit( 0);
+        return 0;
     }
 
 
-    private static void displayAbout() {
+    private static int displayAbout() {
         System.out.println("Svetlana Kosuhina");
         System.out.println("lana__svet@list.ru");
-        System.exit( 0);
+        return 0;
     }
 
+    private static int displayExit() {
+        System.out.println("Terminate program...");
+        return 0;
+    }
 
-    private static void displayError() {
+    private static int displayError() {
         System.out.println("Error! Unknown program argument...");
-        System.exit( -1);
+        return -1;
     }
 
 }
--- src/main/java/kosuhina/tm/constant/TerminalConst.java
+++ src/main/java/kosuhina/tm/constant/TerminalConst.java
@@ -5,5 +5,6 @@ public class TerminalConst {
     public static final String HELP = "help";
     public static final String VERSION = "version";
     public static final String ABOUT = "about";
+    public static final String EXIT = "exit";
 
 }
